6b546042edf855338b9f1f27e99e61e75577af69
tomason <schlosser.tomas@gmail.com>
2014-03-03 10:45:29 +0100

    Fixing GameRegistryBean

--- drooms-tournaments-server/src/main/java/cz/schlosserovi/tomas/drooms/tournaments/beans/GameRegistryBean.java
+++ drooms-tournaments-server/src/main/java/cz/schlosserovi/tomas/drooms/tournaments/beans/GameRegistryBean.java
@@ -13,12 +13,12 @@ import javax.inject.Inject;
 
 import cz.schlosserovi.tomas.drooms.tournaments.data.GameDAO;
 import cz.schlosserovi.tomas.drooms.tournaments.data.GameResultDAO;
-import cz.schlosserovi.tomas.drooms.tournaments.domain.GameResults;
-import cz.schlosserovi.tomas.drooms.tournaments.domain.GameSetup;
-import cz.schlosserovi.tomas.drooms.tournaments.model.Game;
-import cz.schlosserovi.tomas.drooms.tournaments.model.GameResult;
-import cz.schlosserovi.tomas.drooms.tournaments.model.PlaygroundConfig;
-import cz.schlosserovi.tomas.drooms.tournaments.model.Strategy;
+import cz.schlosserovi.tomas.drooms.tournaments.domain.ExecutionResults;
+import cz.schlosserovi.tomas.drooms.tournaments.domain.ExecutionSetup;
+import cz.schlosserovi.tomas.drooms.tournaments.model.GameEntity;
+import cz.schlosserovi.tomas.drooms.tournaments.model.GameResultEntity;
+import cz.schlosserovi.tomas.drooms.tournaments.model.PlaygroundConfigEntity;
+import cz.schlosserovi.tomas.drooms.tournaments.model.StrategyEntity;
 
 @ApplicationScoped
 public class GameRegistryBean {
@@ -28,9 +28,9 @@ public class GameRegistryBean {
     private GameResultDAO resultsDao;
     private final Map<UUID, UUID> inProgress = new HashMap<>();
 
-    public GameSetup getNewGame(UUID id) {
-        Game freeGame = null;
-        for (Game game : gameDao.getUnfinishedGames()) {
+    public ExecutionSetup getNewGame(UUID id) {
+        GameEntity freeGame = null;
+        for (GameEntity game : gameDao.getUnfinishedGames()) {
             if (!inProgress.containsKey(game.getId())) {
                 freeGame = game;
                 break;
@@ -51,13 +51,12 @@ public class GameRegistryBean {
         }
     }
 
-    public void deliverResults(GameResults results) {
-        Game game = gameDao.getGame(results.getGameId());
-        for (GameResult result : resultsDao.getGameResults(game)) {
-            result.setPoints(Integer.parseInt(results.getResults().getProperty(result.getStrategy().getAuthor().getName(), "0")));
-            resultsDao.updateGameResult(result);
+    public void deliverResults(ExecutionResults results) {
+        GameEntity game = gameDao.getGame(results.getGameId());
+        for (GameResultEntity result : resultsDao.getGameResults(game)) {
+            resultsDao.setPoints(result.getId(), Integer.parseInt(results.getResults().getProperty(result.getStrategy().getAuthor().getName(), "0")));
         }
-        gameDao.setFinishedGame(game);
+        gameDao.setFinishedGame(game.getId());
 
         for (UUID key : inProgress.keySet()) {
             if (key.equals(results.getGameId())) {
@@ -66,12 +65,12 @@ public class GameRegistryBean {
         }
     }
 
-    private GameSetup buildGameSetup(Game g) {
+    private ExecutionSetup buildGameSetup(GameEntity g) {
         if (g == null) {
             return null;
         }
 
-        GameSetup result = new GameSetup();
+        ExecutionSetup result = new ExecutionSetup();
         result.setGameId(g.getId());
         result.setClassName("org.drooms.impl.DefaultGame");
         result.setPlayground(g.getPlayground().getSource());
@@ -81,22 +80,21 @@ public class GameRegistryBean {
         return result;
     }
 
-    private Properties buildPlayerSet(List<GameResult> resultSet) {
+    private Properties buildPlayerSet(List<GameResultEntity> resultSet) {
         Properties players = new Properties();
 
-        for (GameResult result : resultSet) {
-            Strategy s = result.getStrategy();
-            players.setProperty(s.getAuthor().getName(),
-                    String.format("%s:%s:%s", s.getGroupId(), s.getArtifactId(), s.getVersion()));
+        for (GameResultEntity result : resultSet) {
+            StrategyEntity s = result.getStrategy();
+            players.setProperty(s.getAuthor().getName(), s.getGav().toString());
         }
 
         return players;
     }
 
-    private Properties buildGameConfig(Collection<PlaygroundConfig> configuration) {
+    private Properties buildGameConfig(Collection<PlaygroundConfigEntity> configuration) {
         Properties result = new Properties();
 
-        for (PlaygroundConfig config : configuration) {
+        for (PlaygroundConfigEntity config : configuration) {
             result.setProperty(config.getKey(), config.getValue());
         }
 
