fe6ce4591e40ae5856e07c228333a9ced4aa9864
tomason <schlosser.tomas@gmail.com>
2014-03-10 21:28:48 +0100

    Ask for server address in casse none was specified (and provide default one)

--- drooms-tournaments-cli/src/main/java/cz/schlosserovi/tomas/drooms/tournaments/client/DroomsCLI.java
+++ drooms-tournaments-cli/src/main/java/cz/schlosserovi/tomas/drooms/tournaments/client/DroomsCLI.java
@@ -8,17 +8,23 @@ import org.jboss.resteasy.spi.ResteasyProviderFactory;
 import cz.schlosserovi.tomas.drooms.tournaments.client.menus.Menu;
 
 public class DroomsCLI {
+    private static final String DEFAULT_DROOMS_SERVER = "tournaments-drooms.rhcloud.com";
 
     public static void main(String[] args) {
         RegisterBuiltin.register(ResteasyProviderFactory.getInstance());
 
-        if (args.length < 1) {
-            System.out.println("Usage: DroomsCLI <drooms-server>");
-            System.out.println("       mvn exec:java -Dexec.args=\"<drooms-server>\"");
-            System.exit(-1);
-        }
-        UserServiceClient client = new UserServiceClient(args[0]);
         Console console = System.console();
+        String serverName = DEFAULT_DROOMS_SERVER;
+        if (args.length < 1) {
+            console.format("What Drooms tournaments server would you like to connect to?%n");
+            String input = console.readLine("URL (%s): ", DEFAULT_DROOMS_SERVER);
+            if (input.trim().length() > 0) {
+                serverName = input.trim();
+            }
+        } else {
+            serverName = args[0];
+        }
+        UserServiceClient client = new UserServiceClient(serverName);
 
         Menu menu = Menu.getMainMenu(console, client);
         while (menu != null) {
