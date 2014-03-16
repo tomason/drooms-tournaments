9b8759c927db651acfcae519372779051ccb01ba
tomason <schlosser.tomas@gmail.com>
2014-03-16 21:56:23 +0100

    Use transaction when inserting new tournament

--- drooms-tournaments-server/src/main/java/cz/schlosserovi/tomas/drooms/tournaments/data/TournamentDAO.java
+++ drooms-tournaments-server/src/main/java/cz/schlosserovi/tomas/drooms/tournaments/data/TournamentDAO.java
@@ -6,6 +6,8 @@ import java.util.Date;
 import java.util.List;
 
 import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
 import javax.enterprise.event.Event;
 import javax.inject.Inject;
 import javax.persistence.criteria.CriteriaBuilder;
@@ -25,10 +27,12 @@ public class TournamentDAO extends AbstractDAO {
     @Inject
     private Event<NewTournamentEvent> newTournament;
 
-    public TournamentEntity insertTournament(String name, Calendar start, Calendar end, int period, Collection<String> playgroundNames) {
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
+    public TournamentEntity insertTournament(String name, Calendar start, Calendar end, int period,
+            Collection<String> playgroundNames) {
         TournamentEntity result = new TournamentEntity(name, start, end, period);
         for (String playgroudName : playgroundNames) {
-            result.addPlayground(playgrounds.getPlayground(playgroudName));
+            result.addPlayground(playgrounds.getPlaygroundWithTournaments(playgroudName));
         }
 
         em.persist(result);
