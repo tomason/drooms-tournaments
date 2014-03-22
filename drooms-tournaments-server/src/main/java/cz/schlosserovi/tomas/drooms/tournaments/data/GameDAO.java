85cabac8722b57a3977754b3dfd7773be6812558
tomason <schlosser.tomas@gmail.com>
2014-03-22 22:27:43 +0100

    Add GameService implementation

--- drooms-tournaments-server/src/main/java/cz/schlosserovi/tomas/drooms/tournaments/data/GameDAO.java
+++ drooms-tournaments-server/src/main/java/cz/schlosserovi/tomas/drooms/tournaments/data/GameDAO.java
@@ -1,5 +1,6 @@
 package cz.schlosserovi.tomas.drooms.tournaments.data;
 
+import java.util.Calendar;
 import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
@@ -8,13 +9,18 @@ import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
 import cz.schlosserovi.tomas.drooms.tournaments.domain.GAV;
 import cz.schlosserovi.tomas.drooms.tournaments.model.GameEntity;
 import cz.schlosserovi.tomas.drooms.tournaments.model.GameResultEntity;
+import cz.schlosserovi.tomas.drooms.tournaments.model.GameStatus;
 import cz.schlosserovi.tomas.drooms.tournaments.model.PlaygroundEntity;
+import cz.schlosserovi.tomas.drooms.tournaments.model.StrategyEntity;
 import cz.schlosserovi.tomas.drooms.tournaments.model.TournamentEntity;
+import cz.schlosserovi.tomas.drooms.tournaments.model.UserEntity;
 
 @Stateless
 public class GameDAO extends AbstractDAO {
@@ -44,19 +50,32 @@ public class GameDAO extends AbstractDAO {
         return result;
     }
 
-    public void setFinishedGame(UUID gameId) {
+    public void setGameInProgress(UUID gameId) {
         GameEntity managed = getGame(gameId);
-        managed.setFinished(true);
+        managed.setStatus(GameStatus.IN_PROGRESS);
+
+        em.merge(managed);
+        em.flush();
+    }
+
+    public void setGameFinished(UUID gameId) {
+        GameEntity managed = getGame(gameId);
+        managed.setStatus(GameStatus.FINISHED);
 
         em.merge(managed);
         em.flush();
     }
 
     public GameEntity getGame(UUID id) {
+        return em.find(GameEntity.class, id);
+    }
+
+    public GameEntity getGameWithResults(UUID id) {
         CriteriaBuilder builder = em.getCriteriaBuilder();
         CriteriaQuery<GameEntity> query = builder.createQuery(GameEntity.class);
 
         Root<GameEntity> game = query.from(GameEntity.class);
+        game.fetch("gameResults");
         query.select(game).where(builder.equal(game.get("id"), id));
 
         return em.createQuery(query).getSingleResult();
@@ -93,12 +112,36 @@ public class GameDAO extends AbstractDAO {
         return em.createQuery(query).getResultList();
     }
 
-    public List<GameEntity> getUnfinishedGames() {
+    public List<GameEntity> getGames(UserEntity user) {
         CriteriaBuilder builder = em.getCriteriaBuilder();
         CriteriaQuery<GameEntity> query = builder.createQuery(GameEntity.class);
-        Root<GameEntity> game = query.from(GameEntity.class);
 
-        query.select(game).where(builder.equal(game.get("finished"), false));
+        Root<GameEntity> game = query.from(GameEntity.class);
+        Join<GameEntity, StrategyEntity> join = game.join("gameResults").join("strategy");
+        query.select(game).where(builder.equal(join.get("author"), user));
+
+        return em.createQuery(query).getResultList();
+    }
+
+    /**
+     * Collects list of games that are either in NEW state or are in IN_PROGRESS
+     * state for more than two hours.
+     * 
+     * @return List of unfinished games.
+     */
+    public List<GameEntity> getUnfinishedGames() {
+        Calendar twoHoursAgo = Calendar.getInstance();
+        twoHoursAgo.add(Calendar.HOUR_OF_DAY, -2);
+        CriteriaBuilder builder = em.getCriteriaBuilder();
+        CriteriaQuery<GameEntity> query = builder.createQuery(GameEntity.class);
+
+        Root<GameEntity> game = query.from(GameEntity.class);
+        game.fetch("gameResults");
+
+        Predicate newGame = builder.equal(game.get("status"), GameStatus.NEW);
+        Predicate inProgress = builder.and(builder.equal(game.get("status"), GameStatus.IN_PROGRESS),
+                builder.lessThan(game.<Calendar> get("lastModified"), twoHoursAgo));
+        query.select(game).where(builder.or(newGame, inProgress));
 
         return em.createQuery(query).getResultList();
     }
---
+++ drooms-tournaments-server/src/main/java/cz/schlosserovi/tomas/drooms/tournaments/services/GameServiceImpl.java
@@ -0,0 +1,70 @@
+package cz.schlosserovi.tomas.drooms.tournaments.services;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
+
+import cz.schlosserovi.tomas.drooms.tournaments.data.GameDAO;
+import cz.schlosserovi.tomas.drooms.tournaments.data.GameResultDAO;
+import cz.schlosserovi.tomas.drooms.tournaments.data.UserDAO;
+import cz.schlosserovi.tomas.drooms.tournaments.domain.Game;
+import cz.schlosserovi.tomas.drooms.tournaments.domain.GameResult;
+import cz.schlosserovi.tomas.drooms.tournaments.model.GameEntity;
+import cz.schlosserovi.tomas.drooms.tournaments.model.GameResultEntity;
+import cz.schlosserovi.tomas.drooms.tournaments.model.UserEntity;
+import cz.schlosserovi.tomas.drooms.tournaments.util.Converter;
+
+public class GameServiceImpl implements GameService {
+    @Inject
+    private UserDAO users;
+    @Inject
+    private GameDAO games;
+    @Inject
+    private GameResultDAO results;
+    @Context
+    private SecurityContext security;
+
+    @Override
+    public Response getGames() {
+        return Response.ok(Converter.forClass(GameEntity.class).convert(games.getGames())).build();
+    }
+
+    @Override
+    public Response getUserGames() {
+        UserEntity user = users.getUser(security.getUserPrincipal().getName());
+        return Response.ok(Converter.forClass(GameEntity.class).convert(games.getGames(user))).build();
+    }
+
+    @Override
+    public Response getExecutionQueue() {
+        List<Game> result = Converter.forClass(GameEntity.class).setRecurseDepth(1).convert(games.getUnfinishedGames());
+        return Response.ok(result).build();
+    }
+
+    @Override
+    public Response reserveExecution(Game game) {
+        games.setGameInProgress(game.getId());
+        return Response.ok().build();
+    }
+
+    @Override
+    public Response deliverResults(Game game) {
+        List<GameResultEntity> resultEntities = results.getGameResults(games.getGame(game.getId()));
+        // There has to be a better way
+        for (GameResultEntity resultEntity : resultEntities) {
+            for (GameResult gameResult : game.getResults()) {
+                if (resultEntity.getStrategy().getGav().equals(gameResult.getStrategy().getGav())) {
+                    results.setPoints(resultEntity.getId(), gameResult.getPoints());
+                    break;
+                }
+            }
+        }
+        games.setGameFinished(game.getId());
+
+        return Response.ok().build();
+    }
+
+}
