355b5f58e29974fc38a1fc6517d04a6b0687c50a
tomason <schlosser.tomas@gmail.com>
2014-03-24 22:58:29 +0100

    Use different approach on updating game results

--- drooms-tournaments-server/src/main/java/cz/schlosserovi/tomas/drooms/tournaments/services/GameServiceImpl.java
+++ drooms-tournaments-server/src/main/java/cz/schlosserovi/tomas/drooms/tournaments/services/GameServiceImpl.java
@@ -1,5 +1,9 @@
 package cz.schlosserovi.tomas.drooms.tournaments.services;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -53,19 +57,36 @@ public class GameServiceImpl implements GameService {
     @Override
     public Response deliverResults(Game game) {
         List<GameResultEntity> resultEntities = results.getGameResults(games.getGame(game.getId()));
-        // There has to be a better way
+        List<GameResult> gameResults = new LinkedList<>(game.getResults());
+        Collections.sort(resultEntities, new GameResultEntityComparator());
+        Collections.sort(gameResults, new GameResultComparator());
+
+        Iterator<GameResult> resultsIterator = gameResults.iterator();
         for (GameResultEntity resultEntity : resultEntities) {
-            for (GameResult gameResult : game.getResults()) {
-                if (resultEntity.getStrategy().getGav().equals(gameResult.getStrategy().getGav())) {
-                    results.setPoints(resultEntity.getId(), gameResult.getPoints());
-                    break;
-                }
+            GameResult current = resultsIterator.next();
+            if (!resultEntity.getStrategy().getGav().equals(current.getStrategy().getGav())) {
+                throw new IllegalArgumentException("Incomplete results were sent");
             }
+            results.setPoints(resultEntity.getId(), current.getPoints());
         }
+
         games.setArtifacts(game.getId(), game.getGameReport(), game.getGameLog());
         games.setGameFinished(game.getId());
 
         return Response.ok().build();
     }
 
+    private static class GameResultEntityComparator implements Comparator<GameResultEntity> {
+        @Override
+        public int compare(GameResultEntity o1, GameResultEntity o2) {
+            return o1.getStrategy().getGav().compareTo(o2.getStrategy().getGav());
+        }
+    }
+
+    private static class GameResultComparator implements Comparator<GameResult> {
+        @Override
+        public int compare(GameResult o1, GameResult o2) {
+            return o1.getStrategy().getGav().compareTo(o2.getStrategy().getGav());
+        }
+    }
 }
