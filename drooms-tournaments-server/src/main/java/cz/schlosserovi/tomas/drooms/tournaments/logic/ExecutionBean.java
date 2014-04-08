26bc9703bb7417a509551369f4c6390022bf8629
tomason <schlosser.tomas@gmail.com>
2014-04-08 19:02:44 +0200

    Add a class for task execution

---
+++ drooms-tournaments-server/src/main/java/cz/schlosserovi/tomas/drooms/tournaments/logic/ExecutionBean.java
@@ -0,0 +1,28 @@
+package cz.schlosserovi.tomas.drooms.tournaments.logic;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import javax.annotation.PreDestroy;
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class ExecutionBean {
+    private ExecutorService executor = Executors.newSingleThreadExecutor();
+
+    @PreDestroy
+    public void disposeExecutor() {
+        executor.shutdown();
+    }
+
+    public Future<?> submit(Runnable task) {
+        return executor.submit(task);
+    }
+
+    public <T> Future<T> submit(Callable<T> task) {
+        return executor.submit(task);
+    }
+
+}
