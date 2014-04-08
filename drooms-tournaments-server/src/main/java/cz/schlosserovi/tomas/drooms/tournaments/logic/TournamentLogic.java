beb745efd3d3b94d31018410eabc4f3f123bfb55
tomason <schlosser.tomas@gmail.com>
2014-04-08 19:04:29 +0200

    Update TournamentResults when game results are submitted

--- drooms-tournaments-server/src/main/java/cz/schlosserovi/tomas/drooms/tournaments/logic/TournamentLogic.java
+++ drooms-tournaments-server/src/main/java/cz/schlosserovi/tomas/drooms/tournaments/logic/TournamentLogic.java
@@ -4,15 +4,22 @@ import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 
+import javax.enterprise.event.Observes;
+import javax.enterprise.event.TransactionPhase;
 import javax.inject.Inject;
 
 import cz.schlosserovi.tomas.drooms.tournaments.data.GameDAO;
 import cz.schlosserovi.tomas.drooms.tournaments.data.PlaygroundDAO;
 import cz.schlosserovi.tomas.drooms.tournaments.data.StrategyDAO;
 import cz.schlosserovi.tomas.drooms.tournaments.data.TournamentDAO;
+import cz.schlosserovi.tomas.drooms.tournaments.data.TournamentResultDAO;
 import cz.schlosserovi.tomas.drooms.tournaments.data.UserDAO;
 import cz.schlosserovi.tomas.drooms.tournaments.domain.Playground;
 import cz.schlosserovi.tomas.drooms.tournaments.domain.Tournament;
+import cz.schlosserovi.tomas.drooms.tournaments.events.GameFinishedEvent;
+import cz.schlosserovi.tomas.drooms.tournaments.events.UpdatedTournamentEvent;
+import cz.schlosserovi.tomas.drooms.tournaments.logic.task.GameResultUpdateTask;
+import cz.schlosserovi.tomas.drooms.tournaments.logic.task.TournamentResultUpdateTask;
 import cz.schlosserovi.tomas.drooms.tournaments.model.GameEntity;
 import cz.schlosserovi.tomas.drooms.tournaments.model.GameResultEntity;
 import cz.schlosserovi.tomas.drooms.tournaments.model.PlaygroundEntity;
@@ -113,6 +120,16 @@ public class TournamentLogic {
         }
 
         tournaments.updateTournament(entity);
+    }
+
+    public static void updateGameResults(@Observes(during = TransactionPhase.AFTER_SUCCESS) GameFinishedEvent event,
+            ExecutionBean executor, GameDAO games, TournamentResultDAO tournamentResults) {
+        executor.submit(new GameResultUpdateTask(games, tournamentResults, event));
+    }
+
+    public static void updateTournamentResults(@Observes(during = TransactionPhase.AFTER_SUCCESS) UpdatedTournamentEvent event,
+            ExecutionBean executor, TournamentResultDAO tournamentResults) {
+        executor.submit(new TournamentResultUpdateTask(tournamentResults, event.getTournament()));
     }
 
     private Converter<TournamentEntity, Tournament> getConverter() {
---
+++ drooms-tournaments-server/src/main/java/cz/schlosserovi/tomas/drooms/tournaments/logic/task/TournamentResultUpdateTask.java
@@ -0,0 +1,56 @@
+package cz.schlosserovi.tomas.drooms.tournaments.logic.task;
+
+import java.math.BigInteger;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.Map.Entry;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+import cz.schlosserovi.tomas.drooms.tournaments.data.TournamentResultDAO;
+import cz.schlosserovi.tomas.drooms.tournaments.model.TournamentResultPartialEntity;
+import cz.schlosserovi.tomas.drooms.tournaments.model.TournamentEntity;
+import cz.schlosserovi.tomas.drooms.tournaments.model.TournamentResultEntity;
+
+public class TournamentResultUpdateTask implements Runnable {
+    private static final BigInteger TWO = new BigInteger("2");
+    private TournamentResultDAO tournamentResults;
+    private TournamentEntity tournament;
+
+    public TournamentResultUpdateTask(TournamentResultDAO tournamentResults, TournamentEntity tournament) {
+        this.tournamentResults = tournamentResults;
+        this.tournament = tournament;
+    }
+
+    @Override
+    public void run() {
+        Collection<TournamentResultEntity> results = tournamentResults.getTournamentResults(tournament);
+        SortedMap<BigInteger, Collection<TournamentResultEntity>> groupedResults = new TreeMap<>();
+        for (TournamentResultEntity tournamentResult : results) {
+            BigInteger sum = new BigInteger("0");
+            for (TournamentResultPartialEntity partialResult : tournamentResult.getPartialResults()) {
+                sum = sum.add(TWO.pow(partialResult.getPosition()));
+            }
+            if (!groupedResults.containsKey(sum)) {
+                groupedResults.put(sum, new LinkedList<TournamentResultEntity>());
+            }
+            groupedResults.get(sum).add(tournamentResult);
+        }
+
+        Collection<TournamentResultEntity> toBeUpdated = new LinkedList<>();
+        int position = 1;
+        for (Entry<BigInteger, Collection<TournamentResultEntity>> resultEntry : groupedResults.entrySet()) {
+            for (TournamentResultEntity resultEntity : resultEntry.getValue()) {
+                // update position only if necessary
+                if (!Integer.valueOf(position).equals(resultEntity.getPosition())) {
+                    resultEntity.setPosition(position);
+                    toBeUpdated.add(resultEntity);
+                }
+            }
+            position += resultEntry.getValue().size();
+        }
+
+        tournamentResults.updateTournamentresults(toBeUpdated);
+    }
+
+}
