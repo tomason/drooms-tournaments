3711c96c6c5bb0445f5146e402b42a14065c602b
tomason <schlosser.tomas@gmail.com>
2014-04-08 18:46:16 +0200

    Add new superclass for NewTournamentEvent

--- drooms-tournaments-server/src/main/java/cz/schlosserovi/tomas/drooms/tournaments/events/NewTournamentEvent.java
+++ drooms-tournaments-server/src/main/java/cz/schlosserovi/tomas/drooms/tournaments/events/NewTournamentEvent.java
@@ -2,14 +2,9 @@ package cz.schlosserovi.tomas.drooms.tournaments.events;
 
 import cz.schlosserovi.tomas.drooms.tournaments.model.TournamentEntity;
 
-public class NewTournamentEvent {
-    private final TournamentEntity tournament;
+public class NewTournamentEvent extends TournamentEvent {
 
     public NewTournamentEvent(TournamentEntity tournament) {
-        this.tournament = tournament;
-    }
-
-    public TournamentEntity getTournament() {
-        return tournament;
+        super(tournament);
     }
 }
---
+++ drooms-tournaments-server/src/main/java/cz/schlosserovi/tomas/drooms/tournaments/events/TournamentEvent.java
@@ -0,0 +1,16 @@
+package cz.schlosserovi.tomas.drooms.tournaments.events;
+
+import cz.schlosserovi.tomas.drooms.tournaments.model.TournamentEntity;
+
+abstract class TournamentEvent {
+    private final TournamentEntity tournament;
+
+    public TournamentEvent(TournamentEntity tournament) {
+        this.tournament = tournament;
+    }
+
+    public TournamentEntity getTournament() {
+        return tournament;
+    }
+
+}
