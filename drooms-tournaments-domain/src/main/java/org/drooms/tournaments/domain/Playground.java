2a838cc46c2ef29785dd9c880a0e9c24e362b694
tomason <schlosser.tomas@gmail.com>
2014-06-19 10:42:37 +0200

    Add equals() to Playground.

--- drooms-tournaments-domain/src/main/java/org/drooms/tournaments/domain/Playground.java
+++ drooms-tournaments-domain/src/main/java/org/drooms/tournaments/domain/Playground.java
@@ -61,6 +61,31 @@ public class Playground implements Comparable<Playground> {
     }
 
     @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Playground other = (Playground) obj;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        return true;
+    }
+
+    @Override
     public int compareTo(Playground o) {
         if (name == null || o.name == null) {
             throw new NullPointerException("Playground name is not set");
