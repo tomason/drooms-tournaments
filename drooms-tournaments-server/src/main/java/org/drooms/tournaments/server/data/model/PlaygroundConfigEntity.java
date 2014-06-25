7cc64167ca551fb1051e24a4bb1fb586699e7ba9
tomason <schlosser.tomas@gmail.com>
2014-06-25 17:25:15 +0200

    Fix bug in Playground configuration.
    Configurations were added to set where, based on equals and hashCode,
    was left only one property (all of them had null id).

--- drooms-tournaments-server/src/main/java/org/drooms/tournaments/server/data/model/PlaygroundConfigEntity.java
+++ drooms-tournaments-server/src/main/java/org/drooms/tournaments/server/data/model/PlaygroundConfigEntity.java
@@ -74,7 +74,7 @@ public class PlaygroundConfigEntity implements Serializable {
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((id == null) ? key.hashCode() : id.hashCode());
         return result;
     }
 
@@ -88,8 +88,7 @@ public class PlaygroundConfigEntity implements Serializable {
             return false;
         PlaygroundConfigEntity other = (PlaygroundConfigEntity) obj;
         if (id == null) {
-            if (other.id != null)
-                return false;
+            return (key == null ? other.key == null : key.equals(other.key));
         } else if (!id.equals(other.id))
             return false;
         return true;
